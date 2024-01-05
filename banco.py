d6ca47724d917fbcf686832f327cbd5a4a87fe94
Leandro_Vitor <leandrovitor455@gmail.com>
2024-01-05 15:54:02 -0300

    transformando depósito, saque e extrato em funções

--- banco.py
+++ banco.py
@@ -1,3 +1,5 @@
+
+
 def main():
     menu = """
     
@@ -9,40 +11,25 @@ def main():
 
     ==> """
 
+
     saldo = 0
     limite = 500
     extrato = ""
     LIMITE_SAQUES = 3
     saques = 0
+
     while(True):
         opcao = input(menu)
 
         if opcao == "d":
-            valor = digitcheck("deposito")
-            extrato += (f"Deposito: + R${valor}\n")
-            saldo += valor
-            print("Depósito efetuado com sucesso!")
+            deposito(extrato, saldo) 
             continue
         elif opcao == "s":
-            valor = digitcheck("saque")
-            if valor > limite:
-                print(f"Valor não deve ultrapassar o limite de R${limite:.02f}")
-                continue
-            if valor > saldo:
-                print(f"Saldo insuficiente. Saldo: R${saldo:.02f}")
-                continue
-            if saques >= LIMITE_SAQUES:
-                print(f"Limite de {LIMITE_SAQUES} saques atingido, tente navamente amanhã")
-                continue
-            saques += 1
-            extrato += (f"Saque:    - R${valor:.02f}\n")
-            saldo -= valor
-            print("Saque efetuado com sucesso!")
+            saque(limite, saldo, LIMITE_SAQUES, saques, extrato)
             continue
                 
         elif opcao == "e":
-            print(extrato)
-            print(f"Saldo atual: R${saldo:.02f}")
+            historico(extrato, saldo)
         elif opcao == "q":
             break
 
@@ -60,4 +47,29 @@ def digitcheck(tipo: str):
         except(ValueError):
             print("Coloque apenas numeros inteiros positivos")
 
+def deposito(extrato, saldo):
+    valor = digitcheck("deposito")
+    extrato += (f"Deposito: + R${valor}\n")
+    saldo += valor
+    print("Depósito efetuado com sucesso!")
+
+def saque(limite, saldo, max, saques, extrato):
+    valor = digitcheck("saque")
+    if valor > limite:
+            print(f"Valor não deve ultrapassar o limite de R${limite:.02f}")
+            return
+    if valor > saldo:
+            print(f"Saldo insuficiente. Saldo: R${saldo:.02f}")
+            return
+    if saques >= limite:
+        print(f"Limite de {limite} saques atingido, tente navamente amanhã")
+        return
+    saques += 1
+    extrato += (f"Saque:    - R${valor:.02f}\n")
+    saldo -= valor
+    print("Saque efetuado com sucesso!")
+
+def historico(extrato, saldo):
+    print(extrato)
+    print(f"Saldo atual: R${saldo:.02f}")
 main()
