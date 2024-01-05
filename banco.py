e6756b578b8159fa920c2fa2fe3640383b8cc901
Leandro_Vitor <leandrovitor455@gmail.com>
2024-01-05 15:31:58 -0300

    feito

--- banco.py
+++ banco.py
@@ -1,33 +1,63 @@
-menu = """
- 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+def main():
+    menu = """
+    
+    [d] Depositar
+    [s] Sacar
+    [e] Extrato
+    [q] Sair
 
 
-==> """
+    ==> """
 
-saldo = 0
-limite = 500
-extrato = ""
-LIMITE_SAQUES = 3
+    saldo = 0
+    limite = 500
+    extrato = ""
+    LIMITE_SAQUES = 3
+    saques = 0
+    while(True):
+        opcao = input(menu)
 
-while(True):
-    opcao = input(menu)
+        if opcao == "d":
+            valor = digitcheck("deposito")
+            extrato += (f"Deposito: + R${valor}\n")
+            saldo += valor
+            print("Depósito efetuado com sucesso!")
+            continue
+        elif opcao == "s":
+            valor = digitcheck("saque")
+            if valor > limite:
+                print(f"Valor não deve ultrapassar o limite de R${limite:.02f}")
+                continue
+            if valor > saldo:
+                print(f"Saldo insuficiente. Saldo: R${saldo:.02f}")
+                continue
+            if saques >= LIMITE_SAQUES:
+                print(f"Limite de {LIMITE_SAQUES} saques atingido, tente navamente amanhã")
+                continue
+            saques += 1
+            extrato += (f"Saque:    - R${valor:.02f}\n")
+            saldo -= valor
+            print("Saque efetuado com sucesso!")
+            continue
+                
+        elif opcao == "e":
+            print(extrato)
+            print(f"Saldo atual: R${saldo:.02f}")
+        elif opcao == "q":
+            break
 
-    if opcao == "d":
-        #deposito()
-        ...
-    elif opcao == "s":
-        #saque()
-        ...
-    elif opcao == "e":
-        #extrato()
-        ...
-    elif opcao == "q":
-        break
 
+        else:
+            print("Operação invalida, por favor selecione novamente o operação desejada")
 
-    else:
-        print("Operação invalida, por favor selecione novamente o operação desejada")
+def digitcheck(tipo: str):
+    while(True):
+        try:
+            digit = int(input(f"Digite o valor para {tipo}: "))
+            if digit > 0:
+                return digit
+            print("Coloque apenas numeros inteiros positivos")
+        except(ValueError):
+            print("Coloque apenas numeros inteiros positivos")
+
+main()
