8aba5d6b26caf3cf4ba654f9b82cf0c9db173e0c
Leandro_Vitor <leandrovitor455@gmail.com>
2024-01-15 17:08:11 -0300

    funções funcionando corretamente

--- banco.py
+++ banco.py
@@ -1,41 +1,60 @@
 
-
+LIMITE_SAQUES = 3
 def main():
-    menu = """
     
-    [d] Depositar
-    [s] Sacar
-    [e] Extrato
-    [q] Sair
-
-
-    ==> """
-
 
     saldo = 0
     limite = 500
     extrato = ""
-    LIMITE_SAQUES = 3
+    
     saques = 0
 
     while(True):
-        opcao = input(menu)
+    
+        opcao = menu()
 
         if opcao == "d":
-            deposito(extrato, saldo) 
+            extrato, saldo = deposito(extrato, saldo)
             continue
         elif opcao == "s":
-            saque(limite, saldo, LIMITE_SAQUES, saques, extrato)
+            data = saque(
+                limite = limite,
+                saldo = saldo,
+                maxi = LIMITE_SAQUES,
+                saques = saques,
+                extrato = extrato
+            )
+            if data != 1:
+                saques += 1
+                extrato = data[0]
+                saldo = data[1]
+            
             continue
                 
         elif opcao == "e":
-            historico(extrato, saldo)
+            historico(saldo, extrato = extrato)
+        
+        
         elif opcao == "q":
             break
 
 
         else:
             print("Operação invalida, por favor selecione novamente o operação desejada")
+
+def menu():
+    print ("""
+=============MENU=============       
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[nc] Nova conta
+[lc] Listar contas
+[nu] Novo usuário
+[q] Sair      
+==============================
+     """)
+    return input("==>  ")
 
 def digitcheck(tipo: str):
     while(True):
@@ -47,29 +66,48 @@ def digitcheck(tipo: str):
         except(ValueError):
             print("Coloque apenas numeros inteiros positivos")
 
-def deposito(extrato, saldo):
+def deposito(extrato, saldo , /):
+    "Efetua o processo de depósito"
+
     valor = digitcheck("deposito")
+
     extrato += (f"Deposito: + R${valor}\n")
     saldo += valor
-    print("Depósito efetuado com sucesso!")
 
-def saque(limite, saldo, max, saques, extrato):
+    print("Depósito efetuado com sucesso!")
+    
+    return extrato, saldo
+
+def saque(*, limite, saldo, maxi, saques, extrato):
+    "Se cumprir com todos os requerimentos, permite ao usuário sacar seu dinheiro da conta"
+
     valor = digitcheck("saque")
+    
     if valor > limite:
             print(f"Valor não deve ultrapassar o limite de R${limite:.02f}")
-            return
+            return 1
     if valor > saldo:
             print(f"Saldo insuficiente. Saldo: R${saldo:.02f}")
-            return
-    if saques >= limite:
+            return 1
+    if saques >= maxi:
         print(f"Limite de {limite} saques atingido, tente navamente amanhã")
-        return
-    saques += 1
+        return 1
+    
+    
     extrato += (f"Saque:    - R${valor:.02f}\n")
     saldo -= valor
+    
+
     print("Saque efetuado com sucesso!")
 
-def historico(extrato, saldo):
+    return extrato, saldo
+
+def historico(saldo, /, *, extrato):
+    "Mostra todas as transações efeituadas pelo usuário"
+
     print(extrato)
     print(f"Saldo atual: R${saldo:.02f}")
+
+    return
+
 main()
